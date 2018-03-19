a21881517fa23721b3260bac7e69473308ab0cec
Hasari <janus.hubert@wp.pl>
2018-03-19 21:34:16 +0100

    Add interface repository

---
+++ RestApi/RestApi.Repository/Interface/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestApi.Repository.Interface
+{
+    interface IRepository<T>
+    {
+        T GetById(string id);
+        IEnumerable<T> GetAll();
+        void Insert(T model);
+        void Update(string id, T model);
+        bool Delete(string id);
+    }
+}
